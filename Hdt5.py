419b83e2431f71acf83e4e76e46d2f8bc34043fb
hid15776 <hid15776@uvg.edu.gt>
2016-08-17 09:47:14 -0600

    Create Hdt5.py

---
+++ Hdt5.py
@@ -0,0 +1,8 @@
+#------------------------------------------------------------------
+#Universidad del Valle de Guatemala
+#Hoja de traba 5. Simulacion, colas
+#Autores: Gladys de la Roca 15755
+#         Jackeline Hidalgo 15776
+#         Luis Sierra 131074
+#Fecha: 22 de agosto de 2016
+#------------------------------------------------------------------
