2dd94e43e600efd6c82426c171895503a292c427
hid15776 <hid15776@uvg.edu.gt>
2016-08-22 17:38:32 -0600

    Update Hdt5.py

--- Hdt5.py
+++ Hdt5.py
@@ -6,3 +6,22 @@
 #         Luis Sierra 131074
 #Fecha: 22 de agosto de 2016
 #------------------------------------------------------------------
+
+import random
+import simpy
+
+RANDOM_SEED = 42
+NEW_CLIENT = 5
+INTERVALO = 10
+minP = 1
+maxP = 3
+
+def source(env, NEW_CLIENT, intervalo, RAM, CPU,WAITING ):
+    """Source generates customers randomly"""
+    for i in range(new_process):
+        instruc = random.randint(1,10)
+        memoria = random.randint(1,10)
+        c = proceso(env, 'proceso%03d' % i, memoria, RAM, CPU, WAITING, instruc)
+        env.process(c)
+        t = random.expovariate(1.0 / intervalo)
+        yield env.timeout(t)
