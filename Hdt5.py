a926b46e572b2695e06689b768feeae450692a85
hid15776 <hid15776@uvg.edu.gt>
2016-08-22 19:27:39 -0600

    Update Hdt5.py

--- Hdt5.py
+++ Hdt5.py
@@ -20,17 +20,19 @@ def source(env, NEW_CLIENT, INTERVALO, RAM, CPU,WAITING ):
     for i in range(NEW_CLIENT):
         instrucciones = random.randint(1,10)
         memoria = random.randint(1,10)
-        c = proceso(env, 'proceso%03d' % i, memoria, RAM, CPU, WAITING, instrucciones)
+        c = cliente(env, 'cliente%03d' % i, memoria, RAM, CPU, WAITING, instrucciones)
         env.process(c)
-        t = random.expovariate(1.0 / intervalo)
+        t = random.expovariate(1.0 / INTERVALO)
         yield env.timeout(t)
-        
-def cliente (env, nombre, memoria, RAM, CPU, WAITING, instrucciones):
+
+def cliente(env, nombre, memoria, RAM, CPU, WAITING, instrucciones):
+    """Customer arrives, is served and leaves."""
     llego = env.now
-    print('%7.4f %s: NEW. Esperando memoria. Entrada' % (llego, nombre)
+    print llego 
+    print('%7.4f %s: NEW. Esperando memoria. Entrada' % (llego, nombre))
 
     with RAM.get(memoria) as req:
-        yield req 
+        yield req #espera que tenga mas memoria
 
         espero = env.now - llego
         print('%7.4f %s: Espero por memoria %6.3f' % (env.now, nombre, espero))
