6bb880dbb56026ce880cb5590923071caf5f6e0a
hid15776 <hid15776@uvg.edu.gt>
2016-08-22 19:18:45 -0600

    Update Hdt5.py

--- Hdt5.py
+++ Hdt5.py
@@ -13,20 +13,19 @@ import simpy
 RANDOM_SEED = 42
 NEW_CLIENT = 5
 INTERVALO = 10
-minP = 1
-maxP = 3
+insMax = 3
 
-def source(env, NEW_CLIENT, intervalo, RAM, CPU,WAITING ):
+def source(env, NEW_CLIENT, INTERVALO, RAM, CPU,WAITING ):
     """Source generates customers randomly"""
     for i in range(NEW_CLIENT):
-        instruc = random.randint(1,10)
+        instrucciones = random.randint(1,10)
         memoria = random.randint(1,10)
-        c = proceso(env, 'proceso%03d' % i, memoria, RAM, CPU, WAITING, instruc)
+        c = proceso(env, 'proceso%03d' % i, memoria, RAM, CPU, WAITING, instrucciones)
         env.process(c)
         t = random.expovariate(1.0 / intervalo)
         yield env.timeout(t)
         
-        def cliente (env, nombre, memoria, RAM, CPU, WAITING, instrucciones):
+def cliente (env, nombre, memoria, RAM, CPU, WAITING, instrucciones):
     llego = env.now
     print('%7.4f %s: NEW. Esperando memoria. Entrada' % (llego, nombre)
 
@@ -41,7 +40,37 @@ def source(env, NEW_CLIENT, intervalo, RAM, CPU,WAITING ):
                 yield reqCPU
                 print('%7.4f %s: RUNNING. Instrucciones %6.3f' % (env.now,nombre,espero))
                 yield env.timeout(1)
-                if instrucciones > 3:
-                    instrucciones = instrucciones - 3
+                if instrucciones > insMax:
+                    instrucciones = instrucciones - insMax
                 else:
                     instrucciones = 0
+                if instrucciones > 0:
+                        siguiente = random.choice(["ready","waiting"])
+                        if siguiente == "waiting":
+                            with WAITING.request() as reqWAITING:
+                                yield reqWAITING
+                                print ('%7.4f %s: WAITING' % (env.now,nombre))
+                                yield env.timeout(1)
+                        print ('%7.4f %s: READY' % (env.now,nombre))
+    tiempoProceso = env.now-llego
+    global tiempoproc
+    tiempoproc = tiempoproc + tiempoProceso
+    print ('%7.4f %s: TERMINATED. Tiempo de ejecucion %s' % (env.now,nombre,tiempoProceso))
+    with RAM.put(memoria) as reqDevolverRAM:
+        yield reqDevolverRAM
+        print ('%7.4f %s: regresando RAM %s' % (env.now,nombre,memoria))
+
+random.seed(RANDOM_SEED)
+env = simpy.Environment()
+
+CPU = simpy.Resource(env, capacity=1)
+RAM= simpy.Container(env,init=100,capacity=100)
+WAITING = simpy.Resource(env,capacity=1)
+env.process(source(env, NEW_CLIENTE,INTERVALO,RAM, CPU,WAITING ))
+tiempoproc = 0
+env.run()# Setup and start the simulation
+
+print('TIEMPO PROMEDIO: %6.3f' % (tiempoproc/NEW_CLIENTE))
+print('DESVIACION STANDARD PROMEDIO: %6.3f' % ())
+
+                    
